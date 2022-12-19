084c0b7ffad698350784da287eff00266c5a7c28
Yusuke.k <shocking.white.0103@gmail.com>
2022-12-20 08:17:35 +0900

    2022-12-20

--- 05_frontend/010_browser_module/start/main.js
+++ 05_frontend/010_browser_module/start/main.js
@@ -1,5 +1,6 @@
 // const myModule = require("./sub.js");
 // myModule.helloFromSub();
 import { helloFromSub } from "./sub.js";
+import { isOdd } from "../node_modules/is-odd/index.js"
 
 helloFromSub();
