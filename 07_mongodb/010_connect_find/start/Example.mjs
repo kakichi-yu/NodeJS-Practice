3e1916ce7077111f1394ff1fbeeae330a1c172f9
Uchan <shocking.white.0103@gmail.com>
2023-01-09 08:24:56 +0900

    2023-01-09

--- 07_mongodb/010_connect_find/start/Example.mjs
+++ 07_mongodb/010_connect_find/start/Example.mjs
@@ -1,29 +1,23 @@
 import env from "dotenv";
 env.config();
 import { MongoClient, ServerApiVersion, } from "mongodb";
-import assert from "assert";
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// client.connect(function (err) {
-//     assert.equal(null, err);
-//     console.log("Connected successfully to server");
-// });
-
-async function getCollection(){
-    try{
+async function getCollection() {
+    try {
         await client.connect();
-        const db =  client.db("bookshelf");
-        return  db.collection("books");
-    }catch{
-        console.log("NG")
+        const db = client.db("bookshelf");
+        return db.collection("books");
+    } catch {
         await client.close();
     }
 }
 
 await getAllBooks();
-async function getAllBooks(){
+async function getAllBooks() {
     const col = await getCollection();
     const cursor = col.find({});
     const result = await cursor.toArray();
