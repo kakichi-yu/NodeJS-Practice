2204309dd8e91b4834675138913004d89c84f409
Uchan <shocking.white.0103@gmail.com>
2023-01-12 08:04:13 +0900

    2023-01-12

--- 09_bookshelf_frontend/server/app.mjs
+++ 09_bookshelf_frontend/server/app.mjs
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import apiRoutes from "./api-routes/index.mjs"
 import "./helpers/db.mjs"
@@ -7,6 +8,7 @@ env.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use("/", express.static("build"))
 app.use(express.json());
 // import cors from "cors";
 // app.use(cors({
@@ -14,6 +16,11 @@ app.use(express.json());
 // }))
 
 app.use("/api", apiRoutes);
+
+app.get("*", function (req, res) {
+    const indexHtml = path.resolve("build", "index.html");
+    res.sendFile(indexHtml);
+})
 
 app.use((req, res) => {
     res.status(404).json({ msg: "Page Not Found" })
